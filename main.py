4f63aec2e2da967c09d0f3858b776f3030b9d6a3
NewBugger <contacts@sometimesnaive.org>
2018-08-05 22:07:36 +0800

    enhancement: remove 'idf_chat' and replace it to 'idf_fromuser'.
    as a private chat(defined by 'filter.private'), value 'chat_id' is
    equvalent to 'from_user.id', so remove necessary var.

--- main.py
+++ main.py
@@ -129,7 +129,7 @@ def process_msg(bot, update):
 		# only when 'conversation' is true
 		if preference_list[str(idf_fromuser)]['conversation'] and not preference_list[str(idf_fromuser)]['blacklist'] :
 			# forward messege to me
-			msg_forward_to_me = bot.forward_message(chat_id=CONFIG['Admin'], from_chat_id=update.message.chat_id, message_id=update.message.message_id)
+			msg_forward_to_me = bot.forward_message(chat_id=CONFIG['Admin'], from_chat_id=idf_fromuser, message_id=update.message.message_id)
 			# add user-info output to me when content is sticker or photo.
 			if msg_forward_to_me.sticker :
 				bot.send_message(chat_id=CONFIG['Admin'], text=LANG['receipt_admin_receive'] % (update.message.from_user.full_name, str(idf_fromuser)), parse_mode=telegram.ParseMode.MARKDOWN, reply_to_message_id=msg_forward_to_me.message_id)
@@ -159,56 +159,58 @@ def process_command(bot, update):
 	command = update.message.text[1:].replace(CONFIG['Username'], '').lower().split()
 	# define 'from_user.id'
 	idf_fromuser = update.message.from_user.id
-	# define 'chat.id'
-	idf_chat = update.message.chat_id
+	## remove 'chat.id' due to the work by 'handlerf_Filters.private'
+	##define 'chat.id'
+	#idf_chat = update.message.chat_id
+	##while equvalent to update.message.from_user.id
 
 	if not preference_list[str(idf_fromuser)]['blacklist'] or idf_fromuser == CONFIG['Admin'] :
 		# bot directives independent to 'conversation' :
 		##bot start
 		if command[0] == 'start' :
-			bot.send_message(chat_id=idf_chat, text=LANG['info_start'])
+			bot.send_message(chat_id=idf_fromuser, text=LANG['info_start'])
 			return
 		##start conversation
 		elif command[0] == 'say' :
 			preference_list[str(idf_fromuser)]['conversation'] = True
-			bot.send_message(chat_id=idf_chat, text=LANG['notify_conversation_start'])
+			bot.send_message(chat_id=idf_fromuser, text=LANG['notify_conversation_start'])
 		##end conversation
 		elif command[0] == 'done' :
 			preference_list[str(idf_fromuser)]['conversation'] = False
-			bot.send_message(chat_id=idf_chat, text=LANG['notify_conversation_end'])
+			bot.send_message(chat_id=idf_fromuser, text=LANG['notify_conversation_end'])
 		##messege-info you point
 		elif command[0] == 'messege_info' :
-			if (idf_fromuser == CONFIG['Admin']) and (idf_chat == CONFIG['Admin']):
+			if (idf_fromuser == CONFIG['Admin']) :
 				if update.message.reply_to_message:
 					if message_list.__contains__(str(update.message.reply_to_message.message_id)):
 						sender_id = message_list[str(update.message.reply_to_message.message_id)]['sender_id']
-						bot.send_message(chat_id=idf_chat, text=LANG['receipt_admin_receive'] % (preference_list[str(sender_id)]['name'], str(sender_id)), parse_mode=telegram.ParseMode.MARKDOWN)
+						bot.send_message(chat_id=idf_fromuser, text=LANG['receipt_admin_receive'] % (preference_list[str(sender_id)]['name'], str(sender_id)), parse_mode=telegram.ParseMode.MARKDOWN)
 					else:
-						bot.send_message(chat_id=idf_chat, text=LANG['error_reply_nodata'])
+						bot.send_message(chat_id=idf_fromuser, text=LANG['error_reply_nodata'])
 			else:
-				bot.send_message(chat_id=idf_chat, text=LANG['warning_user_adminonly'])
+				bot.send_message(chat_id=idf_fromuser, text=LANG['warning_user_adminonly'])
 		##blacklist function to block user
 		elif command[0] == 'block' :
-			if (idf_fromuser == CONFIG['Admin']) and (idf_chat == CONFIG['Admin']):
+			if (idf_fromuser == CONFIG['Admin']) :
 				if update.message.reply_to_message:
 					if message_list.__contains__(str(update.message.reply_to_message.message_id)):
 						preference_list[str(idf_fromuser)]['blacklist'] = True
-						bot.send_message(chat_id=idf_chat, text=LANG['operation_block'])
+						bot.send_message(chat_id=idf_fromuser, text=LANG['operation_block'])
 					else:
-						bot.send_message(chat_id=idf_chat, text=LANG['error_reply_nodata'])
+						bot.send_message(chat_id=idf_fromuser, text=LANG['error_reply_nodata'])
 			else:
-				bot.send_message(chat_id=idf_chat, text=LANG['warning_user_adminonly'])
+				bot.send_message(chat_id=idf_fromuser, text=LANG['warning_user_adminonly'])
 		##blacklist function to unblock user
 		elif command[0] == 'unblock' :
-			if (idf_fromuser == CONFIG['Admin']) and (idf_chat == CONFIG['Admin']):
+			if (idf_fromuser == CONFIG['Admin']) :
 				if update.message.reply_to_message:
 					if message_list.__contains__(str(update.message.reply_to_message.message_id)):
 						preference_list[str(idf_fromuser)]['blacklist'] = False
-						bot.send_message(chat_id=idf_chat, text=LANG['operation_unblock'])
+						bot.send_message(chat_id=idf_fromuser, text=LANG['operation_unblock'])
 					else:
-						bot.send_message(chat_id=idf_chat, text=LANG['error_reply_nodata'])
+						bot.send_message(chat_id=idf_fromuser, text=LANG['error_reply_nodata'])
 			else:
-				bot.send_message(chat_id=idf_chat, text=LANG['warning_user_adminonly'])
+				bot.send_message(chat_id=idf_fromuser, text=LANG['warning_user_adminonly'])
 		# only when 'conversation' false, can operate other bot directives, as to make /done useful
 		elif not preference_list[str(idf_fromuser)]['conversation'] :
 			##receipt switch
@@ -216,20 +218,20 @@ def process_command(bot, update):
 				preference_list[str(idf_fromuser)]['receipt'] = (preference_list[str(idf_fromuser)]['receipt'] == False)
 				threading.Thread(target=save_preference).start()
 				if preference_list[str(idf_fromuser)]['receipt']:
-					bot.send_message(chat_id=idf_chat, text=LANG['info_receipt_on'])
+					bot.send_message(chat_id=idf_fromuser, text=LANG['info_receipt_on'])
 				else:
-					bot.send_message(chat_id=idf_chat, text=LANG['info_receipt_off'])
+					bot.send_message(chat_id=idf_fromuser, text=LANG['info_receipt_off'])
 			##bot version
 			elif command[0] == 'version' :
-				bot.send_message(chat_id=idf_chat, text='Telegram PM Bot to concatnate your conversation between the bot and the sender.\n\nhttps://github.com/NewBugger/telegram-pm-bot')
+				bot.send_message(chat_id=idf_fromuser, text='Telegram PM Bot to concatnate your conversation between the bot and the sender.\n\nhttps://github.com/NewBugger/telegram-pm-bot')
 			else:
 				##when received not existed command
-				bot.send_message(chat_id=idf_chat, text=LANG['warning_user_commandnotfound'])
+				bot.send_message(chat_id=idf_fromuser, text=LANG['warning_user_commandnotfound'])
 		# ask user to /done
 		else:
-			bot.send_message(chat_id=idf_chat, text=LANG['warning_conversation_end'])
+			bot.send_message(chat_id=idf_fromuser, text=LANG['warning_conversation_end'])
 	else:
-		bot.send_message(chat_id=idf_chat, text=LANG['warning_user_blocked'])
+		bot.send_message(chat_id=idf_fromuser, text=LANG['warning_user_blocked'])
 
 
 
